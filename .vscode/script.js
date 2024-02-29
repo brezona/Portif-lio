c7ca28821ec94c3a3a5f4273487284bdf69bf567
brezona <brenogodizila57@gmail.com>
2024-02-29 20:43:11 -0300

    ajuste de letras

--- .vscode/script.js
+++ .vscode/script.js
@@ -12,15 +12,16 @@ function mudar() {
   }
   //a forma simplificada do booleano acima; IF e ELSE; é: html.classList.toggle("branco"); pois "toggle" é uma função que já tem a funcionalidade da que foi criada acima.
 
+
   const img = document.querySelector("#perfil img") //vai selecionar o documento "id perfil img", e o salvará dentro dessa constante "img".
 
   //Se na lista de classe no html conter branco, vai atribuir a constante "img", uma nova imagem e texto
   if (html.classList.contains("branco")) {
-    img.setAttribute("src", "./assets/Breno2.png")
+    img.setAttribute("src","./assets/Breno2.png")
     img.setAttribute("alt","Foto de Breno, com uma parede azul e porta marrom atrás dele")
     //Senão conter branco na lista de classe no html, vai atribuir a constante "img", uma nova imagem e texto; na verade, a que já está no html
   } else {
-    img.setAttribute("src", "./assets/Breno.png")
-    img.setAttribute("alt", "Foto de Breno com camisa social cor de vinho, e ao fundo há um portão e uma escadaria")
+    img.setAttribute("src","./assets/Breno.png")
+    img.setAttribute("alt","Foto de Breno com camisa social cor de vinho, e ao fundo há um portão e uma escadaria")
   }
 }
